69368be6cde012ca36ecb670831b96363b0a6268
Johannes Kastl <git@johannes-kastl.de>
2026-02-14 15:44:11 +0100

    ZZ_update_all_git_submodules.sh: use while-loop to get list of submodules; fail if ./helpers/update_and_commit_git_module.sh does not exist...

--- ZZ_update_all_git_submodules.sh
+++ ZZ_update_all_git_submodules.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-for module in \
-    helpers \
-    ansible/roles/*
+[[ -e ./helpers/update_and_commit_git_module.sh ]] || {
+    echo "./helpers/update_and_commit_git_module.sh does not exist, do you need to run 'git submodule update' first?"
+    exit 3
+}
+
+while read -r module
 do
+
     ./helpers/update_and_commit_git_module.sh "${module}"
-done
+
+done < <(awk '/path = / {print $3}' .gitmodules)
 
 exit 0
